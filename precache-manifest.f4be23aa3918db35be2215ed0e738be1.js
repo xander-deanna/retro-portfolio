f4bf84a4047dc84909d37f19af474e76fc2c73eb
Deanna <xander.deanna@gmail.com>
2021-05-24 18:22:57 -0500

    manual deploy attempt

---
+++ precache-manifest.f4be23aa3918db35be2215ed0e738be1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c291792bc92232a91d2452971507d55a",
+    "url": "/retro-portfolio/index.html"
+  },
+  {
+    "revision": "26c0dca3ba9cc90506ad",
+    "url": "/retro-portfolio/static/css/main.1ff90b93.chunk.css"
+  },
+  {
+    "revision": "1c3d9da92ccaca82ca80",
+    "url": "/retro-portfolio/static/js/2.82f17694.chunk.js"
+  },
+  {
+    "revision": "164f9532e5a3153b726a0315e59dc489",
+    "url": "/retro-portfolio/static/js/2.82f17694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c0dca3ba9cc90506ad",
+    "url": "/retro-portfolio/static/js/main.2281cbc0.chunk.js"
+  },
+  {
+    "revision": "f27b6c361ff1a33c16ed",
+    "url": "/retro-portfolio/static/js/runtime-main.082c683f.js"
+  },
+  {
+    "revision": "8605ab9a7bae16d572b85f0b6b5b6ba3",
+    "url": "/retro-portfolio/static/media/about-me-photo.8605ab9a.gif"
+  },
+  {
+    "revision": "1c806c20a02d9534b75d0f98cbe84a53",
+    "url": "/retro-portfolio/static/media/aim-buddy.1c806c20.gif"
+  },
+  {
+    "revision": "05a5132d433fcf81d51b4b5b759f3181",
+    "url": "/retro-portfolio/static/media/background-image.05a5132d.gif"
+  },
+  {
+    "revision": "beb0f6b87c81a32971e4e69404f2937e",
+    "url": "/retro-portfolio/static/media/brush.beb0f6b8.ico"
+  },
+  {
+    "revision": "e89b9e7c33f8317bd8f141fdde3ae57f",
+    "url": "/retro-portfolio/static/media/code-quiz.e89b9e7c.gif"
+  },
+  {
+    "revision": "c05df51c8afbfcd6f5a5f837636521ce",
+    "url": "/retro-portfolio/static/media/employee-directory.c05df51c.gif"
+  },
+  {
+    "revision": "f658dd415baa59383b6c6127c0b6fb2c",
+    "url": "/retro-portfolio/static/media/mail.f658dd41.ico"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/retro-portfolio/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/retro-portfolio/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  },
+  {
+    "revision": "46a818165c333cc08a2188c990761fe0",
+    "url": "/retro-portfolio/static/media/password-generator.46a81816.gif"
+  },
+  {
+    "revision": "ccc2eeda78833e8725ded4cf49a4868d",
+    "url": "/retro-portfolio/static/media/project-1.ccc2eeda.gif"
+  },
+  {
+    "revision": "cd81d165fe34c5b3fa371d393345db20",
+    "url": "/retro-portfolio/static/media/project-2.cd81d165.gif"
+  },
+  {
+    "revision": "cbb357bfa7d0d8d9e0dfbb95ff78af6e",
+    "url": "/retro-portfolio/static/media/weather-dashboard.cbb357bf.gif"
+  },
+  {
+    "revision": "7a56402d73eaacac4503f335fa358e89",
+    "url": "/retro-portfolio/static/media/windows95_startup_theme.7a56402d.mp3"
+  }
+]);
--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-import { Howl } from 'howler';
-
-export const startupSound = () => {
-  return new Howl({
-    src: [require('../assets/windows95_startup_theme.mp3')],
-  });
-};
